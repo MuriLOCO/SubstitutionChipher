ad2821d7a71a78942285912ed3f1ea71ac742943
ezmenjo <jose.murilo.mendes@ericsson.com>
2018-03-08 15:32:39 -0500

    Removed unsused code and added popup for invalid key.

--- src/main/java/com/concordia/SubstitutionCipher/frontend/SubstitutionController.java
+++ src/main/java/com/concordia/SubstitutionCipher/frontend/SubstitutionController.java
@@ -1,24 +1,28 @@
 package com.concordia.SubstitutionCipher.frontend;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import com.concordia.SubstitutionCipher.enums.DecryptMethod;
 import com.concordia.SubstitutionCipher.utils.CipherUtils;
 import com.concordia.SubstitutionCipher.utils.GeneralUtils;
 import com.concordia.SubstitutionCipher.wrappers.CryptanalysisWrapper;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-public class SubstitutionController implements Initializable {
+public class SubstitutionController {
 
   private static final String FAST_METHOD = "Fast Method";
   private static final String DECRYPT_AND_EVALUATE_METHOD = "Decrypt and Evaluate Method";
@@ -72,11 +76,38 @@ public class SubstitutionController implements Initializable {
       taCipherTextDecrypt.setText("");
 
     } else if (event.getTarget() == btnEncryption || event.getTarget().toString().contains("Encrypt")) {
+      if (!GeneralUtils.validateKey(tfKeyEncrypt.getText())) {
+
+        final Stage dialogStage = new Stage();
+        final GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        final Scene scene = new Scene(grid, 300, 150);
+        dialogStage.setScene(scene);
+        dialogStage.setTitle("Error");
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+
+        final Label lab_alert = new Label("Problems with the key!");
+        grid.add(lab_alert, 0, 1);
+
+        final Button btbOk = new Button("Ok I got it ;) I will fix");
+        btbOk.setOnAction(new EventHandler<ActionEvent>() {
+
+          @Override
+          public void handle(final ActionEvent arg0) {
+            dialogStage.hide();
+          }
+        });
+        grid.add(btbOk, 0, 2);
+        dialogStage.show();
+
+      } else {
       adaptedText = GeneralUtils.adaptString(taPlainTextEncrypt.getText());
-      adaptedKey = GeneralUtils.adaptString(tfKeyEncrypt.getText());
-      GeneralUtils.validate(adaptedText, adaptedKey);
+        adaptedKey = GeneralUtils.adaptString(tfKeyEncrypt.getText());
       String result = CipherUtils.encrypt(adaptedText, adaptedKey);
       taCipherTextEncrypt.setText(result);
+      }
 
     } else if (event.getTarget() == btnDecryption || event.getTarget().toString().contains("Decrypt")) {
       adaptedText = GeneralUtils.adaptString(taCipherTextDecrypt.getText());
@@ -89,10 +120,4 @@ public class SubstitutionController implements Initializable {
       taPlainTextDecrypt.setText(result.getText());
     }
   }
-
-  @Override
-  public void initialize(URL url, ResourceBundle rb) {
-
-  }
-
 }
--- src/main/java/com/concordia/SubstitutionCipher/utils/GeneralUtils.java
+++ src/main/java/com/concordia/SubstitutionCipher/utils/GeneralUtils.java
@@ -1,39 +1,34 @@
 package com.concordia.SubstitutionCipher.utils;
 
+import com.concordia.SubstitutionCipher.enums.BaseVariables;
+
 public class GeneralUtils {
 
   /**
-   * Validade the key and the text
-   * @param text - Plain text
-   * @param key - Chosen key
+  * Validade the key
+  * @param key - Chosen key
    */
-  public static void validate(final String text, final String key) {
-    //This is the English Alphabet defined in a Z26 (Upper case)
-    final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";    
-    
-     if (key.length() != 26)
-      throw new RuntimeException("The key must have 26 characters");
-    
+  public static boolean validateKey(final String key) {
     //This checks if the key has repeatable characters
-    for (int i = 0; i < alphabet.length(); i++) {
+    for (int i = 0; i < BaseVariables.ALPHABET_ENGLISH.getValue().length(); i++) {
       int count = 0;
       for (int j = 0; j < key.length(); j++) {
-        if (alphabet.charAt(i) == key.charAt(j))
+        if (BaseVariables.ALPHABET_ENGLISH.getValue().charAt(i) == key.charAt(j))
           count++;
       }
-      if (count > 1) {
-        throw new RuntimeException("The key must not have repeateble characters.");
-      }
+      if (count > 1 || key.length() != 26)
+        return false;
     }
+    return true;
   }
-  
+
   /**
    * Deletes numbers, special characters, spaces and makes upper case of a String
    * @param string
    * @return clean String
    */
   public static String adaptString(final String string) {
-	  return string.replaceAll("[^a-zA-Z]+","").trim().toUpperCase();
-  }  
-  
+    return string.replaceAll("[^a-zA-Z]+", "").trim().toUpperCase();
+  }
+
 }
